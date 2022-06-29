03452165115231b8fc6f9cbd9d48de1ff6ccfad9
Phabliny <phablinymartins@gmail.com>
2022-06-29 10:55:07 -0300

    Mudando o nome da logo JPW

--- jpw_app/src/Header.js
+++ jpw_app/src/Header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
       <div>
         <header class="row m-0 p-0 border">
           <div class="col col-6 d-flex flex-column justify-content-center align-items-center py-lg-5 py-sm-3 border">
-            <img class="col-sm-12 col-md-12 col-10 logo" src="../img/logo_jpw.png" />
+            <img class="col-sm-12 col-md-12 col-10 logo" src="../img/jpw.png" />
             <h2 class="col-sm-12 col-md-11 pt-3 text-justify">PREÇO JUSTO COM ATENDIMENTO DIFERENCIADO EM FERRAGENS EM UBERLÂNDIA E REGIÃO?</h2>
             <a href='https://wa.me/5534984417989?text=Desejo%20fazer%20um%20orçamento' target="_blank" rel="noopener noreferrer"><img src='../img/zap.png' class='logozap'></img></a>
           </div>
