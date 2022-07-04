eb9960f2ccfcd8d4e1a1568ec2bf0262e816b8e0
Phabliny <phablinymartins@gmail.com>
2022-07-04 16:13:04 -0300

    Adicionando o novo flip

--- jpw_app/src/Main.js
+++ jpw_app/src/Main.js
@@ -4,13 +4,13 @@ import Section_produtos from './sections_main/Section_produtos'
 import Section_marcas from './sections_main/Section_marcas'
 import Section_loja_movel from './sections_main/Section_loja_movel'
 import Section_formulario from './sections_main/Section_formulario'
-import Teste from './Teste'
+import Flip from './Flip'
 
 export default class Main extends Component {
   render() {
     return (
       <main>
-          <Teste/>
+          <Flip/>
           <Section_empresa/>
           <Section_produtos/>
           <Section_marcas/>
--- jpw_app/src/index.js
+++ jpw_app/src/index.js
@@ -1,7 +1,6 @@
 import App from "./App"
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.css'
 
-//ReactDOM.render(<App/>, document.getElementById('root'))
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
+ReactDOM.render(<App/>, document.getElementById('root'))
