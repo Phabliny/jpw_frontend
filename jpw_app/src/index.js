9105078c6d7df66610ccc576f89eb661fef77ebc
Phabliny <phablinymartins@gmail.com>
2022-07-04 14:56:50 -0300

    teste flip

--- jpw_app/src/index.js
+++ jpw_app/src/index.js
@@ -1,6 +1,7 @@
 import App from "./App"
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.css'
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+//ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
