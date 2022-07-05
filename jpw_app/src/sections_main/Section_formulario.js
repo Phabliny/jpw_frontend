8fd8e4d0312e3bf9c6c0d7a92df12a267d8ba884
Breno Eduardo <64919093+BrenoEduardo@users.noreply.github.com>
2022-07-05 14:52:32 -0300

    Update Section_formulario.js

--- jpw_app/src/sections_main/Section_formulario.js
+++ jpw_app/src/sections_main/Section_formulario.js
@@ -5,7 +5,7 @@ export default class Section_formulario extends Component {
     render() {
         return (
             <div class="container py-sm-1 py-md-3 py-lg-5" id="formulario">
-                <div class="d-flex flex-column justify-content-center align-items-center py-4">
+                <div class="d-flex flex-column justify-content-center align-items-center py-4 py-sm-1 py-md-3" >
                     <h1>POSSO TE AJUDAR?</h1>
                     <h2>(ENTRE EM CONTATO CONOSCO PARA FAZERMOS UM ORÇAMENTO)</h2>
                     <NovoCliente />
