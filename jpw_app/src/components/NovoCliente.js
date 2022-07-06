f74da1ae8aba37accaba27ba556d7bb44891885c
Phabliny <phablinymartins@gmail.com>
2022-07-06 16:17:12 -0300

    telefone

--- jpw_app/src/components/NovoCliente.js
+++ jpw_app/src/components/NovoCliente.js
@@ -43,11 +43,11 @@ const NovoCliente = () => {
         text: "O campo nome precisa ter mais de 1 letra, favor preenche-lo corretamente",
         confirmButtonColor: "#ffa600e6",
       });
-    } else if ((cliente.telefone.length < 10)||(cliente.telefone.length > 12)){
+    } else if (cliente.telefone.length > 12){
       swal.fire({
         title: "Algo deu errado",
         icon: "error",
-        text: "O campo telefone está coma quantidade de dígitos incorreta, favor preenche-lo corretamente",
+        text: "O campo telefone está com a quantidade de dígitos incorreta, favor preenche-lo corretamente",
         confirmButtonColor: "#ffa600e6",
       });
     } else if (!document.getElementById("chekbox").checked) {
