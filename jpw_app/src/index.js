399e883e7811e9de50d45cfd397fbe12eb599a36
Phabliny <phablinymartins@gmail.com>
2022-10-05 17:45:02 -0300

    Várias modificações no front-end

--- jpw_app/src/index.js
+++
@@ -1,6 +0,0 @@
-import App from "./App"
-import ReactDOM from "react-dom"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/style.css'
-
-ReactDOM.render(<App/>, document.getElementById('root'))
